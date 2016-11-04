b71d837c062357ed1f177eeec71f52cb5498bf10
Atul-Nambudiri <atul.nambudiri@gmail.com>
2016-11-04 06:33:23 -0500

    Updated to latest API version

--- Android/IOSMS/app/src/main/java/com/atulnambudiri/iosms/MainActivityFragment.java
+++ Android/IOSMS/app/src/main/java/com/atulnambudiri/iosms/MainActivityFragment.java
@@ -1,13 +1,21 @@
 package com.atulnambudiri.iosms;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Base64;
 import android.util.Log;
@@ -19,6 +27,8 @@ import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,11 +36,12 @@ import java.io.IOException;
 public class MainActivityFragment extends Fragment {
     final int SELECT_IMAGE = 12;
     TextView textToDisplay;
-    static String phoneNumber = "TWILIONUMBER";
+    static String phoneNumber = "6304513493";
     static String numberBufferStart = "$$-";
     static String numberBufferEnd = "-$$";
     static int numberBufferLength = 10;
     static int smsLength = 160;
+    private boolean canSendMessages;
 
     public MainActivityFragment() {
     }
@@ -40,6 +51,10 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         textToDisplay = (TextView) rootView.findViewById(R.id.image_text);
+        canSendMessages = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+        if(!canSendMessages) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
         return rootView;
     }
 
@@ -54,6 +69,7 @@ public class MainActivityFragment extends Fragment {
         if (requestCode == SELECT_IMAGE) {
             if (resultCode == Activity.RESULT_OK) {
                 Uri chosenImage = data.getData();
+                String imageName = getFileNameFromURI(getActivity(), chosenImage);
                 Bitmap chosenBitmap;
                 try {
                     chosenBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), chosenImage);
@@ -62,17 +78,34 @@ public class MainActivityFragment extends Fragment {
                     return;
                 }
                 String imageText = bitmapToString(chosenBitmap);
-                //String imageText = encodeTest("Hello my name is Atul Nambudiri. How is it going? This is just random text that I am typing. Do not worry about the content of this text. It is of no concern to you. You must only make sure that it is arriving at the destination correctly, and in the correct order. In order to accomplish this, I need some text that I know the correct order for. That is why I typed up this whole long sentance. Just to let you know. Okay this is enough. I have to stop this. Hello. Goodbye. dlafkghlsdfjgsdlfjklskdjfglksdfjghksdjglkjsdfhgljkdsfhgklhsrituherilgbeiruvbierihgeirthilerbiguhrighiergilergjkaegkuegsbeguksebgukb");
+                //imageText = encodeTest("This is a test string. Lets see what happens.");
                 Log.d("ImageText", imageText);
                 if(imageText.length() > 18000) {
-                    textToDisplay.setText("Image is too large to send as text. Image length is: " + Integer.toString(imageText.length()));
+                    textToDisplay.setText("Image " + imageName + " is too large to send as text. Image length is: " + Integer.toString(imageText.length()));
                 }
                 else {
-                    textToDisplay.setText("Sending Image. Image length is: " + Integer.toString(imageText.length()));
-                    new SendSMSTask().execute(imageText);
+                    textToDisplay.setText("Sending Image " + imageName + ". Image length is: " + Integer.toString(imageText.length()));
+                    new SendSMSTask().execute(imageText, imageName);
                 }
             }
         }
+    }
+
+    public String getFileNameFromURI(Context context, Uri uri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(context, uri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+
+        Pattern pattern = Pattern.compile(".*\\/(.*)");
+        Matcher matcher = pattern.matcher(result);
+        if(matcher.find()) {
+            return matcher.group(1);
+        }
+        return result;
     }
 
     public String encodeTest(String str) {
@@ -93,7 +126,8 @@ public class MainActivityFragment extends Fragment {
         @Override
         protected String doInBackground(String... params) {
             String imageText = params[0];
-            String sentMessages = sendImage(imageText);
+            String imageName = params[1];
+            String sentMessages = sendImage(imageText, imageName);
             return sentMessages;
         }
 
@@ -101,9 +135,9 @@ public class MainActivityFragment extends Fragment {
             textToDisplay.setText("Image has been sent. " + result + " messages were sent");
         }
 
-        public String sendImage(String imageText) {
+        public String sendImage(String imageText, String imageName) {
             int sentCounter = 0;
-            sendText("Start Image");
+            sendText("Start Image. Image Name: " + imageName);
             try {
                 Thread.sleep(1000); //Sleep for a bit so that the initial message has some time to be sent
             } catch (InterruptedException e) {
@@ -130,8 +164,21 @@ public class MainActivityFragment extends Fragment {
         }
 
         public void sendText(String message) {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            Log.d("Can Send Messages", Boolean.toString(canSendMessages));
+            if(canSendMessages) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            }
+        }
+    }
+
+    public void onRequestPermissionResult(int requstCode, String[] permissions, int[] grantResults) {
+        switch (requstCode) {
+            case PackageManager.PERMISSION_GRANTED: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    canSendMessages = true;
+                }
+            }
         }
     }
 
